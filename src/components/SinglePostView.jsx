6c198da419c25d6823e299befbc3825838df2ef3
RLjumber <ronaldjumber@gmail.com>
2023-02-01 11:36:15 -0500

    stuck at single post views

--- src/components/SinglePostView.jsx
+++ src/components/SinglePostView.jsx
@@ -6,39 +6,7 @@ const COHORT_NAME = '2209-FTB-ET-WEB-PT';
 
 const SinglePostView = () => {
 
-    const [posts, setPosts] = useState([]);
-    const { id } = useParams();
     
-    useEffect(() => {
-        fetch(`${BASE_URL}/${COHORT_NAME}/posts`)
-          .then(response => response.json())
-          .then(result => {
-            setPosts(result.data.posts);
-            console.log('this is my data', result.data.posts);
-          })
-          .catch(console.error);
-      }, []);
-
-    
-    const currentPost = posts.filter(post => post._id === id)
-
-    console.log("currentPost: ", currentPost)
-
-    
-  return (
-    <div>
-        <ul>
-            {currentPost.map(post => {
-                <li>{post.title}</li>
-            })}
-        </ul>
-        <span>
-            <Link to="/posts">
-                <button>Back to All Posts</button>
-            </Link>
-        </span>
-    </div>
-  )
 }
 
 export default SinglePostView;
